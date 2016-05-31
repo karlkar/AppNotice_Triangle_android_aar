13ceb2c5192ea4117b6668c5578c72c8f0bf78b5
Steven Overson <soverson@ghostery.com>
2016-05-31 12:42:32 -0600

    BOO-8420: - Add build 39 of the SDK. - Add code to detect when the hybrid menu screen is displayed without the SDK being initialized and display a message. - Manifest-less SDK Implementation. - Bold the in-app privacy option on the hybrid menu to aid testing.

--- Triangle_aar/src/main/java/com/ghostery/privacy/triangle_aar/HybridPrivacySettings.java
+++ Triangle_aar/src/main/java/com/ghostery/privacy/triangle_aar/HybridPrivacySettings.java
@@ -1,12 +1,16 @@
 package com.ghostery.privacy.triangle_aar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.ghostery.privacy.appnoticesdk.AppNotice;
+
 public class HybridPrivacySettings extends AppCompatActivity {
+    private final static String TAG = "HybridPrivacySettings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,14 @@ public class HybridPrivacySettings extends AppCompatActivity {
         pref_in_app_privacy_layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(App.getContext(), "Manage Privacy Preferences from hybrid.", Toast.LENGTH_LONG).show();
-                MainActivity.getAppNotice().showManagePreferences();
+                AppNotice appNotice = MainActivity.getAppNotice();
+                if (appNotice != null) {
+                    Toast.makeText(App.getContext(), "Manage Privacy Preferences from hybrid.", Toast.LENGTH_LONG).show();
+                    appNotice.showManagePreferences();
+                } else {
+                    Toast.makeText(App.getContext(), "The App Notice SDK has not been initialized in this session. Please restart the app and try again.", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "The App Notice SDK has not been initialized in this session.");
+                }
             }
         });
     }
